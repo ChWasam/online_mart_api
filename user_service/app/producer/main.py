202c96d3ee9a2926bb6dab45eb8e1a7af2e04cd7
ChWasam <wasam.developer@gmail.com>
2024-07-19 23:55:21 +0500

    updated

--- user_service/app/producer/main.py
+++ user_service/app/producer/main.py
@@ -69,57 +69,14 @@ async def register_user(register:Annotated[model.RegisterUser,Depends(model.Regi
 
 
 
-# user login 
-@app.post("/user/login")
-async def login_user(login:Annotated[OAuth2PasswordRequestForm,Depends(OAuth2PasswordRequestForm)]):
-    user_proto = user_pb2.User(username = login.username, password = login.password, option = user_pb2.SelectOption.LOGIN)
-    serialized_user = user_proto.SerializeToString()
-    await kafka.produce_message(settings.KAFKA_TOPIC, serialized_user)
-
-    user_proto = await kafka.consume_message_response()
-
-    if user_proto.error_message or user_proto.http_status_code :
-        raise HTTPException(status_code=user_proto.http_status_code, detail=user_proto.error_message)
-    return {"access_token":user_proto.access_token, "token_type":"bearer", "refresh_token":user_proto.refresh_token}
 
 
 
-#  user/me
-@app.get("/user/me")
-async def get_current_user(verify_token:Annotated[str,Depends(auth.verify_access_token)]):
-    credentials_exception = HTTPException(status_code=401, 
-    detail="Could not validate credentials",
-    headers={"WWW-Authenticate": "Bearer"}   
-    )
-    if not verify_token:
-        raise credentials_exception
-    user_proto = user_pb2.User(username = verify_token, option = user_pb2.SelectOption.CURRENT_USER)
-    serialized_user = user_proto.SerializeToString()
-    await kafka.produce_message(settings.KAFKA_TOPIC, serialized_user)
-
-    user_proto = await kafka.consume_message_response()
-
-    if user_proto.error_message or user_proto.http_status_code:
-        raise credentials_exception
-    return user_proto.username
 
 
-@app.post("/user/refresh_token")
-async def refresh_token(old_refresh_token:str):
-    credentials_exception = HTTPException(status_code=401, 
-    detail="Could not validate credentials",
-    headers={"WWW-Authenticate": "Bearer"} 
-    )
-    email = auth.verify_refresh_token(old_refresh_token)
-    if not email:
-        raise credentials_exception
-    user_proto = user_pb2.User(email = email, option = user_pb2.SelectOption.REFRESH_TOKEN)
-    serialized_user = user_proto.SerializeToString()
-    await kafka.produce_message(settings.KAFKA_TOPIC, serialized_user)
-    user_proto = await kafka.consume_message_response()
-    if user_proto.error_message or user_proto.http_status_code :
-        raise credentials_exception
-    return {"access_token":user_proto.access_token, "token_type":"bearer", "refresh_token":user_proto.refresh_token}
+
+
+
 
 
 
